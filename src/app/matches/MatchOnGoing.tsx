1c106a8cebc76db3f1ea3e1856c7def1352a619d
Kataleko <katalekoweb@gmail.com>
2026-05-11 15:47:52 +0100

    feat: start ongoing match page

--- src/app/matches/MatchOnGoing.tsx
+++ src/app/matches/MatchOnGoing.tsx
@@ -5,10 +5,26 @@ import React from "react";
 import { ScrollView, Text, View } from "react-native";
 
 const MatchOnGoing = () => {
+
+  const maskedWord = ['J', 'O', '_', 'O']
+  const tip = "Algo para se divertir"
+
   return (
-    <ScrollView className="flex-1 p-2 gap-4">
-      <View>
-        <Text>Match em andamento </Text>
+    <ScrollView className="flex-1 p-2 pt-6">
+      <View className="gap-6 items-center">
+        <Text className="text-text font-regular text-lg text-center underline">
+          {tip}
+        </Text>
+
+        <View className="flex-row flex-wrap gap-2 items-center justify-center">
+          {maskedWord.map(letter => (
+            <View className="border-b-4 h-9 w-9 border-text">
+            <Text className="text-text font-bold text-2xl text-center underline">
+              {letter === '_' ? '' : letter}
+            </Text>
+          </View>
+          ))}
+        </View>
       </View>
     </ScrollView>
   );
